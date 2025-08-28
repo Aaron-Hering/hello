3ce0feaaf39b81245d85d007e175bbc54a57077b
Aaron Heringcd CS1/Hacks/hellocd ..cd ../workspacescd workspacescd CS1 <ahering3@huskers.unl.edu>
2025-08-28 09:10:11 -0500

    changed text to add minor

--- hello.c
+++ hello.c
@@ -6,8 +6,9 @@
 
  int main(int argc, char **argv) {
 
-    printf("Hello, I'm\n");
-    printf("Major\n");
+    printf("Hello, I'm Aaron Hering\n");
+    printf("Major is Computer Engineering\n");
+    printf("Minor could be math\n");
 
     return 0;
  }
